baccdde5db10e71ed6385441f52b44a7b91ff131
Sajag Shrestha <sajagshrestha0852@gmail.com>
2021-05-10 19:35:52 +0545

    animate with requestAnimationFrame v1

--- js/index.js
+++ js/index.js
@@ -7,37 +7,93 @@ const nextButton = document.getElementById("next");
 
 const numberOfImages = images.length;
 const imageSize = images[0].clientWidth;
+
+let posX = 0;
 let counter = 1;
 
+function slideForward(current, destination) {
+	const animate = () => {
+		if (current >= destination) {
+			imageContainer.style.transform = `translateX(-${destination}px)`;
+			return (posX = destination);
+		}
+		current += 60;
+		imageContainer.style.transform = `translateX(-${current}px)`;
+
+		window.requestAnimationFrame(() => animate());
+	};
+	window.requestAnimationFrame(animate);
+}
+
+function slideBackward(current, destination) {
+	const animate = () => {
+		if (current <= destination) {
+			imageContainer.style.transform = `translateX(-${destination}px)`;
+			return (posX = destination);
+		}
+		current -= 60;
+		imageContainer.style.transform = `translateX(-${current}px)`;
+		window.requestAnimationFrame(() => animate());
+	};
+	window.requestAnimationFrame(animate);
+}
+
 const slideNextPicture = () => {
-	//if we reach last image
 	if (counter === numberOfImages) {
-		//go to first image
-		imageContainer.style.transform = `translateX(${0}px)`;
+		slideBackward(posX, 0);
+		posX = 0;
 		counter = 1;
 	} else {
-		//else move to next image
-		imageContainer.style.transform = `translateX(-${
-			imageSize * counter
-		}px)`;
+		let destinationPosX = imageSize * counter;
+		slideForward(posX, destinationPosX);
+		posX = destinationPosX;
 		counter++;
 	}
-	console.log(counter);
 };
 
-const slidePrevPicture = () => {
+const slidePreviousPicture = () => {
 	if (counter === 1) {
-		imageContainer.style.transform = `translateX(-${
-			imageSize * (numberOfImages - 1)
-		}px)`;
+		let destinationPosX = imageSize * (numberOfImages - 1);
+		slideForward(posX, destinationPosX);
+		posX = destinationPosX;
 		counter = numberOfImages;
 	} else {
-		imageContainer.style.transform = `translateX(-${
-			imageSize * (counter - 2)
-		}px)`;
+		let destinationPosX = imageSize * (counter - 2);
+		slideBackward(posX, destinationPosX);
+		posX = destinationPosX;
 		counter--;
 	}
-	console.log(counter);
 };
+// const slideNextPicture = () => {
+// 	//if we reach last image
+// 	if (counter === numberOfImages) {
+// 		//go to first image
+// 		imageContainer.style.transform = `translateX(${0}px)`;
+// 		counter = 1;
+// 	} else {
+// 		//else move to next image
+// 		imageContainer.style.transform = `translateX(-${
+// 			imageSize * counter
+// 		}px)`;
+// 		counter++;
+// 	}
+// 	console.log(counter);
+// };
+
+// const slidePrevPicture = () => {
+// 	if (counter === 1) {
+// 		imageContainer.style.transform = `translateX(-${
+// 			imageSize * (numberOfImages - 1)
+// 		}px)`;
+// 		counter = numberOfImages;
+// 	} else {
+// 		imageContainer.style.transform = `translateX(-${
+// 			imageSize * (counter - 2)
+// 		}px)`;
+// 		counter--;
+// 	}
+// 	console.log(counter);
+// };
+
 nextButton.addEventListener("click", slideNextPicture);
-prevButton.addEventListener("click", slidePrevPicture);
+prevButton.addEventListener("click", slidePreviousPicture);
